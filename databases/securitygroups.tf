f39cb0c8ac76c30043561aab305608fa2fc3860a
kamil.herbik <kamil.herbikl@dataio.pl>
2017-06-10 14:46:08 +0200

    Separate directory for each project

---
+++ databases/securitygroups.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow www inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_web_to_db" {
+  name        = "allow_web_to_db"
+  description = "Allow www traffic to db"
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.allow_web.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
